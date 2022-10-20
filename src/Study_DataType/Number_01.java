925a32250f5d5095c41c2876dcc45b96da13bdd6
vega_laptop <ldh1255@naver.com>
2022-10-20 10:31:53 +0900

    자바 공부를 위한 첫 커밋

---
+++ src/Study_DataType/Number_01.java
@@ -0,0 +1,12 @@
+package Study_DataType;
+
+public class Number_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a = "a";
+		System.out.println(a);
+		
+	}
+
+}
