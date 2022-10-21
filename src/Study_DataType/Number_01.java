ad3edade1925bcb774c12343c716c872a6420f39
vega_laptop <ldh1255@naver.com>
2022-10-21 11:10:43 +0900

    블로그 정리 및 내용 추가

--- src/Study_DataType/Number_01.java
+++ src/Study_DataType/Number_01.java
@@ -20,9 +20,9 @@ public class Number_01 {
 		// equals
 		// 두개의 문자열이 동일한지를 비교하여 결과값을 리턴
 		// 예시1
-		String equals1 = "hello";
+		String equals1 = "hi";
 		String equals2 = "java";
-		String equals3 = "hello";
+		String equals3 = "hi";
 		System.out.println(equals1.equals(equals2)); // false 출력
 		System.out.println(equals1.equals(equals3)); // true 출력
 		
@@ -30,8 +30,8 @@ public class Number_01 {
 		// equals1 과 equals3 이 같기 때문에 true
 		
 		// 예시2
-		String equals4 = "hello";
-		String equals5 = new String("hello");
+		String equals4 = "hi";
+		String equals5 = new String("hi");
 		System.out.println(equals4.equals(equals5)); // true
 		System.out.println(equals4 == equals5); // false
 		
@@ -40,12 +40,55 @@ public class Number_01 {
 		// 값은 같더라도 서로 다른 객체이고, == 는 동일한 자료형의 판별에 사용하는 연산자이기 때문에 false 로 리턴한다.
 		
 		
-		
 		// indexOf
 		// 문자열에서 특정 문자가 시작되는 위치(인덱스)를 리턴
 		// 예시1
-		String indexOf1 = "Hello Java";
+		String indexOf1 = "hi Java";
 		System.out.println(indexOf1.indexOf("Java")); // 6 출력
+		
+		
+		// contains
+		// 문자열에서 특정 문자열이 포함되어 있는지의 여부를 리턴
+		// 예시 1
+		String contain1 = "Hi Java";
+		System.out.println(contain1.contains("Java")); // true 출력
+		
+		
+		
+		// charAt
+		// 문자열에서 특정 위치의 문자(char)를 리턴
+		// 예시 1
+		String charAt1 = "Hi Java";
+		System.out.println(charAt1.charAt(6)); // "J" 출력
+		
+		
+		
+		// replaceAll
+		// 문자열 중 특정 문자열을 다른 문자열로 바꾸고자 할 때 사용
+		// 예시 1
+		String replaceAll1 = "Hi Java";
+		System.out.println(replaceAll1.replaceAll("Java", "World")); // Hi World 출력
+		
+		
+		// substring
+		// 문자열 중 특정 부분을 뽑아낼 경우 사용
+		// 예시 1
+		String substring1 = "Hi Java";
+		System.out.println(substring1.substring(0, 4)); // Hi J 출력
+		
+		
+		// toUpperCase
+		// 문자열을 모두 대문자로 변경할 때 사용
+		// 예시 1
+		String toUpperCase1 = "Hi Java";
+		System.out.println(toUpperCase1.toUpperCase()); // HI JAVA 출력
+		
+		
+		// split
+		// 문자열을 특정 구분자로 분리하는 메소드
+		// 예시 1
+		String split1 = "a:b:c:d";
+		String[] result1 = split1.split(":");  // result는 {"a", "b", "c", "d"}
 		
 	}
 
