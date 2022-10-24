f0af6795ba1d7f5450d3d86517d48a16615e4262
vega_laptop <ldh1255@naver.com>
2022-10-24 09:53:09 +0900

    객체지향 프로그래밍 연습문제 풀기 완료

--- src/Do_Test/T1_Test.java
+++ src/Do_Test/T1_Test.java
@@ -82,6 +82,103 @@ class MaxLimitCalculator extends Calculator {
 	
 }
 
+
+
+// 문제 6
+class Calculator_06 {
+    int value; // Integer > int
+    void add(int val) {
+        this.value += val;
+    }
+
+    public int getValue() { // Integer > int
+        return this.value;
+    }
+}
+
+
+
+// 문제 7
+interface Mineral {
+	int getpoint();
+}
+
+class Gold implements Mineral {
+	public int getpoint() {
+		return 100;
+	}
+}
+
+class Silver implements Mineral {
+	public int getpoint() {
+		return 90;
+	}
+}
+
+class Bronze implements Mineral {
+	public int getpoint() {
+		return 80;
+	}
+}
+
+class MineralCalculator {
+    int value = 0;
+
+    public void add(Mineral mineral) {
+        this.value += 100;
+    }
+
+    public void add(Silver silver) {
+        this.value += 90;
+    }
+
+    public void add(Bronze bronze) {
+        this.value += 80;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+}
+
+
+
+// 문제 8
+interface Predator_08 {
+}
+
+class Animal_08 {
+}
+
+class Dog_08 extends Animal_08 {
+}
+
+class Lion_08 extends Animal_08 implements Predator_08 {
+}
+
+
+
+// 문제 9
+interface Predator_09 {
+    String bark();
+}
+
+abstract class Animal_09 {
+    public String hello() {
+        return "hello";
+    }
+}
+
+class Dog_09 extends Animal_09 {
+}
+
+class Lion_09 extends Animal_09 implements Predator_09 {
+    public String bark() {
+        return "Bark bark!!";
+    }
+}
+
+
 public class T1_Test {
 
 	public static void main(String[] args) {
@@ -132,14 +229,69 @@ public class T1_Test {
 		// public class Sample {
 		// 		public static void main(String[] args) {
 		// 			ArrayList<Integer> a = new ArrayList<>(Arrays.asList(1, 2, 3));
+		// == 배열리스트 숫자형태 a 변수 정의 1, 2, 3
 		//			ArrayList<Integer> b = a;
+		// == b = a?
 		//			a.add(4);
+		// == a 변수에 4 추가
 		//			System.out.println(b.size());
+		// == b 변수의 배열리스트 크기(사이즈)?
 		//		}
 		//	}
 		
-		// 예측 출력 : 
+		// 나의 예상 출력 : 1, 2, 3
+		// 이유 : a 변수에 추가 시 프로그램 순서에 따른 b 변수에 영향을 미치지 않음으로 b 의 출력값은 a 의 초기값이 된다.
+		
+		
+		// 문제 6
+		// 다음은 Calculator 객체를 생성하고 값을 더한 후에 그 결과값을 출력하는 예제이다. 하지만 코드를 실행하면 오류가 발생한다.
+		// Calculator 클래스를 수정하여 다음의 코드에서 오류가 발생하지 않도록 하시오.
+        Calculator_06 cal = new Calculator_06();
+        cal.add(3);  // 여기서 NullPointerException 이 발생한다.
+        System.out.println(cal.getValue());
+        
+        // Integer 의 잘못된 표기로 int 로 변경후 실행하면 정상 작동
+		
+        
+        // 문제 7 못풀었다 : 문제 이해 못함
+        // 다음은 광물의 가치를 계산하는 MineralCalculator 클래스와 그 사용법이 담긴 코드이다.
+        // 광물 계산기는 금인 경우 100, 은인 경우 90, 구리의 경우는 80의 가치를 더하는 기능(add 메스드)이 있다.
+        // 하지만 이 광물 계산기는 광물이 추가될 때마다 add 메서드를 추가해야 한다는 단점이 있다.
+        // 광물이 추가되더라도 MineralCalculator 클래스를 변경할 필요가 없도록 코드를 수정하시오.
+        MineralCalculator cal_07 = new MineralCalculator();
+        cal_07.add(new Gold());
+        cal_07.add(new Silver());
+        cal_07.add(new Bronze());
+        System.out.println(cal_07.getValue());  // 270 출력
+        
+        
+        // 문제 8
+        // 다음의 코드를 보고 1번, 2번, 3번, 4번, 5번 문장 중에서 오류가 발생하는 문장을 모두 찾고 오류의 원인에 대해서 설명하시오.
+        Animal_08 a_08 = new Animal_08();  // 1번 pass
+        Animal_08 b_08 = new Dog_08();  // 2번 pass
+        Animal_08 c_08 = new Lion_08();  // 3번 pass
+        Dog_08 d_08 = new Animal_08();  // 4번 error
+        // 원인 : 자식 클래스가 부모 클래스를 간섭하지 못한다.
+        Predator_08 e_08 = new Lion_08();  // 5번 pass?
+        
+        
+        // 문제 9
+        // 다음의 코드를 보고 1번, 2번, 3번, 4번, 5번, 6번 문장 중에서
+        // 오류가 발생하는 문장을 모두 찾고 오류의 원인에 대해서 설명하시오.
+        Animal_09 a_09 = new Lion_09();
+        Lion_09 b_09 = new Lion_09();
+        Predator_09 c_09 = new Lion_09();
 
+        System.out.println(a_09.hello());  // 1번 pass
+        System.out.println(a_09.bark());   // 2번 error
+        // 원인 : 인터페이스 클래스는 클래스 지정 후 사용 가능?
+        System.out.println(b_09.hello());  // 3번 pass
+        System.out.println(b_09.bark());   // 4번 pass
+        System.out.println(c_09.hello());  // 5번 error
+        // 원인 : 추상클래스인 Animal은 인터페이스 클래스로 불러온 객체의 값을 가져올 수 없다?
+        System.out.println(c_09.bark());   // 6번 pass
+        
+        
 	}
 
 }
